d96470fb3a93927fcd970f51b369b90249f4fe49
Chee Yi Ong <cheeyi@gmail.com>
2019-12-13 18:00:42 +0800

    Introduce BleOperationType sealed class with associated classes

---
+++ app/src/main/java/com/punchthrough/blestarterappandroid/ble/BleOperationType.kt
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2019 Punch Through Design LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.punchthrough.blestarterappandroid.ble
+
+import android.bluetooth.BluetoothDevice
+import java.util.UUID
+
+/** Abstract sealed class representing a type of BLE operation */
+sealed class BleOperationType
+
+/** Connect to [device] and perform service discovery */
+data class Connect(val device: BluetoothDevice) : BleOperationType()
+
+/** Disconnect from [device] and release all connection resources */
+data class Disconnect(val device: BluetoothDevice) : BleOperationType()
+
+/** Write [payload] as the value of a characteristic represented by [characteristicUuid] */
+data class CharacteristicWrite(
+    val device: BluetoothDevice,
+    val characteristicUuid: UUID,
+    val payload: ByteArray
+) : BleOperationType() {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as CharacteristicWrite
+
+        if (device != other.device) return false
+        if (characteristicUuid != other.characteristicUuid) return false
+        if (!payload.contentEquals(other.payload)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = device.hashCode()
+        result = 31 * result + characteristicUuid.hashCode()
+        result = 31 * result + payload.contentHashCode()
+        return result
+    }
+}
+
+/** Read the value of a characteristic represented by [characteristicUuid] */
+data class CharacteristicRead(
+    val device: BluetoothDevice,
+    val characteristicUuid: UUID
+) : BleOperationType()
+
+/** Write [payload] as the value of a descriptor represented by [descriptorUuid] */
+data class DescriptorWrite(
+    val device: BluetoothDevice,
+    val descriptorUuid: UUID,
+    val payload: ByteArray
+) : BleOperationType() {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as DescriptorWrite
+
+        if (device != other.device) return false
+        if (descriptorUuid != other.descriptorUuid) return false
+        if (!payload.contentEquals(other.payload)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = device.hashCode()
+        result = 31 * result + descriptorUuid.hashCode()
+        result = 31 * result + payload.contentHashCode()
+        return result
+    }
+}
+
+/** Read the value of a descriptor represented by [descriptorUuid] */
+data class DescriptorRead(
+    val device: BluetoothDevice,
+    val descriptorUuid: UUID
+) : BleOperationType()
+
+/** Enable notifications/indications on a characteristic represented by [characteristicUuid] */
+data class EnableNotifications(
+    val device: BluetoothDevice,
+    val characteristicUuid: UUID
+) : BleOperationType()
+
+/** Disable notifications/indications on a characteristic represented by [characteristicUuid] */
+data class DisableNotifications(
+    val device: BluetoothDevice,
+    val characteristicUuid: UUID
+) : BleOperationType()
+
+/** Request for an MTU of [mtu] */
+data class MtuRequest(
+    val device: BluetoothDevice,
+    val mtu: Int
+) : BleOperationType()
